775ebd4d3dfaf767bab6c6ce965540b6066bd6ba
alimikegami <alimikegami1@gmail.com>
2025-08-11 20:51:59 +0800

    test(user-service): add more test cases for integration tests

--- user-service/test/create_user_test.go
+++ user-service/test/create_user_test.go
@@ -7,29 +7,88 @@ import (
 	"net/http"
 
 	"github.com/alimikegami/pos-microservices/user-service/internal/dto"
-	"github.com/stretchr/testify/require"
+	"github.com/labstack/echo/v4"
 )
 
-func (s *IntegrationTestSuite) TestCreateUser() {
-	payload := dto.UserRequest{
-		Name:     "Test",
-		Email:    "test3@gmail.com",
-		Password: "1234",
-		RoleID:   1,
+func (s *IntegrationTestSuite) Test_CreateUser() {
+	type TestCase struct {
+		Name           string
+		Request        dto.UserRequest
+		ExpectedStatus int
+		AssertResponse func(s *IntegrationTestSuite, resp *http.Response)
 	}
 
-	jsonPayload, err := json.Marshal(payload)
-	require.NoError(s.T(), err)
+	testCases := []TestCase{
+		{
+			Name: "Valid request",
+			Request: dto.UserRequest{
+				Name:     "test",
+				Email:    "test@gmail.com",
+				Password: "123456",
+				RoleID:   1,
+			},
+			ExpectedStatus: http.StatusOK,
+			AssertResponse: func(s *IntegrationTestSuite, resp *http.Response) {
+			},
+		},
+		{
+			Name: "Missing email",
+			Request: dto.UserRequest{
+				Name:     "test",
+				Password: "123456",
+				RoleID:   1,
+			},
+			ExpectedStatus: http.StatusBadRequest,
+			AssertResponse: func(s *IntegrationTestSuite, resp *http.Response) {
+			},
+		},
+		{
+			Name: "Missing name",
+			Request: dto.UserRequest{
+				Email:    "test@gmail.com",
+				Password: "123456",
+				RoleID:   1,
+			},
+			ExpectedStatus: http.StatusBadRequest,
+			AssertResponse: func(s *IntegrationTestSuite, resp *http.Response) {
+			},
+		},
+		{
+			Name: "Invalid email",
+			Request: dto.UserRequest{
+				Name:     "test",
+				Email:    "test",
+				Password: "123456",
+				RoleID:   1,
+			},
+			ExpectedStatus: http.StatusBadRequest,
+			AssertResponse: func(s *IntegrationTestSuite, resp *http.Response) {
+			},
+		},
+	}
 
-	createUserURL := fmt.Sprintf("http://localhost:%s/api/v1/users/register", s.app.Config.ServicePort)
-	req, err := http.NewRequest("POST", createUserURL, bytes.NewBuffer(jsonPayload))
-	require.NoError(s.T(), err)
+	for _, tc := range testCases {
+		s.Run(tc.Name, func() {
+			reqBody, err := json.Marshal(tc.Request)
+			s.NoError(err)
 
-	req.Header.Set("Content-Type", "application/json")
+			req, err := http.NewRequest(http.MethodPost,
+				fmt.Sprintf("http://localhost:%s/api/v1/users/register", s.app.Config.ServicePort),
+				bytes.NewBuffer(reqBody),
+			)
+			s.NoError(err)
 
-	resp, err := http.DefaultClient.Do(req)
-	require.NoError(s.T(), err)
-	defer resp.Body.Close()
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 
-	s.Equal(http.StatusCreated, resp.StatusCode)
+			client := http.Client{}
+			resp, err := client.Do(req)
+			s.NoError(err)
+
+			s.Equal(tc.ExpectedStatus, resp.StatusCode)
+
+			if tc.AssertResponse != nil {
+				tc.AssertResponse(s, resp)
+			}
+		})
+	}
 }
--- user-service/test/user_login_test.go
+++ user-service/test/user_login_test.go
@@ -7,27 +7,72 @@ import (
 	"net/http"
 
 	"github.com/alimikegami/pos-microservices/user-service/internal/dto"
-	"github.com/stretchr/testify/require"
+	"github.com/labstack/echo/v4"
 )
 
-func (s *IntegrationTestSuite) TestUserLogin() {
-	payload := dto.UserRequest{
-		Email:    "testuser@example.com",
-		Password: "testpassword",
+func (s *IntegrationTestSuite) Test_UserLogin() {
+	type TestCase struct {
+		Name           string
+		Request        dto.UserRequest
+		ExpectedStatus int
+		AssertResponse func(s *IntegrationTestSuite, resp *http.Response)
 	}
 
-	jsonPayload, err := json.Marshal(payload)
-	require.NoError(s.T(), err)
+	testCases := []TestCase{
+		{
+			Name: "Valid request",
+			Request: dto.UserRequest{
+				Email:    "admin@example.com",
+				Password: "123456",
+			},
+			ExpectedStatus: http.StatusOK,
+			AssertResponse: func(s *IntegrationTestSuite, resp *http.Response) {
+			},
+		},
+		{
+			Name: "Wrong password",
+			Request: dto.UserRequest{
+				Email:    "test@gmail.com",
+				Password: "1234",
+			},
+			ExpectedStatus: http.StatusUnauthorized,
+			AssertResponse: func(s *IntegrationTestSuite, resp *http.Response) {
+			},
+		},
+		{
+			Name: "Invalid email",
+			Request: dto.UserRequest{
+				Email:    "test",
+				Password: "123456",
+			},
+			ExpectedStatus: http.StatusNotFound,
+			AssertResponse: func(s *IntegrationTestSuite, resp *http.Response) {
+			},
+		},
+	}
 
-	createUserURL := fmt.Sprintf("http://localhost:%s/api/v1/users/login", s.app.Config.ServicePort)
-	req, err := http.NewRequest("POST", createUserURL, bytes.NewBuffer(jsonPayload))
-	require.NoError(s.T(), err)
+	for _, tc := range testCases {
+		s.Run(tc.Name, func() {
+			reqBody, err := json.Marshal(tc.Request)
+			s.NoError(err)
 
-	req.Header.Set("Content-Type", "application/json")
+			req, err := http.NewRequest(http.MethodPost,
+				fmt.Sprintf("http://localhost:%s/api/v1/users/login", s.app.Config.ServicePort),
+				bytes.NewBuffer(reqBody),
+			)
+			s.NoError(err)
 
-	resp, err := http.DefaultClient.Do(req)
-	require.NoError(s.T(), err)
-	defer resp.Body.Close()
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 
-	s.Equal(http.StatusOK, resp.StatusCode)
+			client := http.Client{}
+			resp, err := client.Do(req)
+			s.NoError(err)
+
+			s.Equal(tc.ExpectedStatus, resp.StatusCode)
+
+			if tc.AssertResponse != nil {
+				tc.AssertResponse(s, resp)
+			}
+		})
+	}
 }
